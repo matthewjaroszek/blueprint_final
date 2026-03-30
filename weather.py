fe758ec9bf7fef5158fd5e4853f23a3a10acdc82
matthewjaroszek <matthewj369@icloud.com>
2026-03-30 12:40:32 -0400

    env test

--- weather.py
+++ weather.py
@@ -1,16 +1,30 @@
 import pandas as pd
 
 def summary(csv, n):
-    print(csv, "\n")
+    print(csv,  "\n")
     df = pd.read_csv(csvs[csv])
+    print(f'Num rows: {df.shape[0]}')
+    print(f'Num cols: {df.shape[1]}', "\n")
     cols = df.columns
+    x = 0
     for col in cols:
-        print(col, " - ", end="")
-    print("\n\n", df.head(n))
+        x += 1
+        print(col, end = "")
+        if x >= 5:
+            print("\n")
+            x = 0
+        else:
+            print(" - ", end = "")
+    
+    print("\b\b  \n\n", df.head(n))
+
+def summarizeAll(n):
+    for csv in csvs:
+        summary(csv, n)
 
 csvs = {
-    "recent_caps.csv" : "GlobalWeatherRepository.csv.gz"
+    "recentCaps" : "GlobalWeatherRepository.csv.gz",
+    "histDC" : "dc_weather.csv.gz"
 }
 
-for csv in csvs:
-    summary(csv, 3)
+summarizeAll(3)

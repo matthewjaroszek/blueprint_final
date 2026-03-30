90c76b676f3e7e7eca351c6a8dec39ef24e1687a
matthewjaroszek <matthewj369@icloud.com>
2026-03-30 11:39:33 -0400

    summary

--- weather.py
+++ weather.py
@@ -1,1 +1,16 @@
-#hi
+import pandas as pd
+
+def summary(csv, n):
+    print(csv, "\n")
+    df = pd.read_csv(csvs[csv])
+    cols = df.columns
+    for col in cols:
+        print(col, " - ", end="")
+    print("\n\n", df.head(n))
+
+csvs = {
+    "recent_caps.csv" : "GlobalWeatherRepository.csv.gz"
+}
+
+for csv in csvs:
+    summary(csv, 3)

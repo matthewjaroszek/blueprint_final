9160b9225786dc7a77c4d571386f14f2af929fde
matthewjaroszek <matthewj369@icloud.com>
2026-03-31 03:49:17 -0400

    update

--- config.py
+++ config.py
@@ -3,32 +3,41 @@ from dotenv import load_dotenv
 import os
 import sqlite3 as sql
 import psycopg2 as pgs
+import sys, argparse
 
-load_dotenv()
 print()
+load_dotenv()
+sql_url = os.getenv('sql_url')
 
-csvs = {
-    "rc" : "rc.gz",
-    "dc" : "dc.gz",
-    "trc" : "trc.gz",
-    "tdc" : "tdc.gz",
+gzs = {
+    "rc" : "gzs/rc.gz",
+    "dc" : "gzs/dc.gz",
+    "trc" : "gzs/trc.gz",
+    "tdc" : "gzs/tdc.gz",
 }
 
-def getdf(csv):
-    return pd.read_csv(csvs[csv])
+dbs = {
+    "rc" : "dbs/rc.db",
+    "dc" : "dbs/dc.db",
+    "trc" : "dbs/trc.db",
+    "tdc" : "dbs/tdc.db",
+}
 
-def summarize(csvs, r = 0, c = 0):
-    if isinstance(csvs, str):
-        print(f'{csvs}: ', end = "")
-        summarize(getdf(csvs), r, c)
+def getdf(gz):
+    return pd.read_csv(gzs[gz])
+
+def summarize(gzs, r = 0, c = 0):
+    if isinstance(gzs, str):
+        print(f'{gzs}: ', end = "")
+        summarize(getdf(gzs), r, c)
     
-    elif isinstance(csvs, dict) or isinstance(csvs, set):
-        for csv in csvs:
-            summarize(csv, r, c)
+    elif isinstance(gzs, dict) or isinstance(gzs, set):
+        for gz in gzs:
+            summarize(gz, r, c)
 
-    elif isinstance(csvs, pd.DataFrame):
-        print(f'{csvs.shape[0]} x {csvs.shape[1]} ', '-'*185, '\n')
-        cols = csvs.columns
+    elif isinstance(gzs, pd.DataFrame):
+        print(f'{gzs.shape[0]} x {gzs.shape[1]} ', '-'*185, '\n')
+        cols = gzs.columns
         x = 0
         for col in cols:
             x += 1
@@ -41,36 +50,36 @@ def summarize(csvs, r = 0, c = 0):
         print("\b\b  ")
         print()
         if (r*c > 0):
-            print("", csvs.iloc[:r, :c], "\n")
+            print("", gzs.iloc[:r, :c], "\n")
 
-def nc(csv, old, new):
-    df = getdf(csv)
+def nc(gz, old, new):
+    df = getdf(gz)
     df = df.rename(columns={old: new})
-    df.to_csv(csvs[csv], index = False)
+    df.to_csv(gzs[gz], index = False)
 
-def drop(csv, col):
-    df = getdf(csv)
+def drop(gz, col):
+    df = getdf(gz)
     df = df.drop(col, axis = 1)
-    df.to_csv(csvs[csv], index = False)
-    return getdf(csv)
+    df.to_csv(gzs[gz], index = False)
+    return getdf(gz)
 
-def print_sql(x, csvs, b = True):
+def print_sql(x, dbs, b = True):
     if (not b):
         return
-    print(f"{csvs}", "-"*35, "\n")
+    print(f"{dbs}", "-"*35, "\n")
     x = x.fetchall()
     for r in x:
         print(r)
     print()
 
-def run_sql(csvs, ex, b = True):
-    if (isinstance(csvs, str)):
-        conn = sql.connect(f'{csvs}.db')
+def run_sql(dbs, ex, b = True):
+    if (isinstance(dbs, str)):
+        conn = sql.connect(f'./dbs/{dbs}.db')
         x = conn.cursor()
-        x.execute(ex.format(csvs))
-        print_sql(x, csvs, b)
+        x.execute(ex.format(dbs))
+        print_sql(x, dbs, b)
         conn.close()
     
-    elif isinstance(csvs, dict) or isinstance(csvs, set):
-        for csv in csvs:
-            run_sql(csv, ex, b)
+    elif isinstance(dbs, dict) or isinstance(dbs, set):
+        for db in dbs:
+            run_sql(db, ex, b)
--- load.py
+++ load.py
@@ -2,19 +2,22 @@ from config import *
 
 n = 50
 df = getdf('rc').head(n)
-df.to_csv("trc.gz", index = False)
+df.to_csv(gzs['rc'], index = False)
 df = getdf('dc').head(n)
-df.to_csv("tdc.gz", index = False)
+df.to_csv(gzs['dc'], index = False)
 
-for csv in csvs:
-    df = getdf(csv)
-    conn = sql.connect(f'{csv}.db')
-    df.to_sql(f'{csv}', conn, if_exists='replace')
+for gz in gzs:
+    df = getdf(gz)
+    conn = sql.connect(f'./dbs/{gz}.db')
+    df.to_sql(f'{gz}', conn, if_exists='replace')
     x = conn.cursor()
-    x.execute(f'DROP INDEX IF EXISTS ix_{csv}_index')
-    x.execute(f'ALTER TABLE {csv} DROP COLUMN "index"')
+    x.execute(f'DROP INDEX IF EXISTS ix_{gz}_index')
+    x.execute(f'ALTER TABLE {gz} DROP COLUMN "index"')
     conn.commit()
     conn.close()
 
-summarize({'rc', 'dc'}, 5, 0)
-run_sql({'rc', 'dc'}, "PRAGMA table_info({})", True)
+if len(sys.argv) >= 2:
+    if (int)(sys.argv[1]) == 1:
+        summarize({'rc', 'dc'}, 5, 0)
+    if len(sys.argv) >= 3 and (int)(sys.argv[2]) == 1:
+        run_sql({'rc', 'dc'}, "PRAGMA table_info({})", True)

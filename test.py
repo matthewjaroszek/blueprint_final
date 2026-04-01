33c8907dfaf663634f74ad7cc4832395c5937a4f
matthewjaroszek <matthewj369@icloud.com>
2026-04-01 17:18:58 -0400

    done cleaning

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
 from config import *
+
+copy_gz("historical_dc_original", "historical_dc_copy")
+dfo = get_df("historical_dc_copy")
+dfc = get_df("historical_dc_original")
+load_db(dfc, 'historical_dc_copy')
